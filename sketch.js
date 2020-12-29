867366f8157cf1f1ad3aa490a1109a2097f3c581
rebecablanco <74179691+rebecablanco@users.noreply.github.com>
2020-12-29 17:59:57 +0100

    Update sketch.js
    Detect hand gestures + draw as a curve + erase function
    
    Co-Authored-By: Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let handpose;
 let video;
 let predictions = [];
-let points = [];
+
+let points = []; // array of drawn points
+let eraseStart = 0; // counts frames since we entered the erase zone
 
 function setup() {
   createCanvas(640, 480);
@@ -25,26 +27,61 @@ function modelReady() {
 }
 
 function draw() {
+  // invert the webcam
+  translate(video.width, 0);
+  scale(-1, 1);
+
+  // show the video
   image(video, 0, 0, width, height);
 
-  // Call the findKeypoints function to push all keypoints into array
-  findKeypoints();
+  // erase zone:
+  noStroke();
+  fill(255, 0, 0);
+  rect(0, 0, 80, 80);
 
-  // Draw lines between keypoints within the array
-  for (let i = 0; i < points.length - 1; i+=1){
+  // detect which fingers are up
+  let gesture = findGesture();
 
-    /* CODE TO DRAW ELLIPSES
-    noStroke();
-    ellipse(arr[i][0], arr[i][1], 10); */
-
-    if (points.length > 2){
-      let nextindex = i + 1; 
-      stroke(255, 204, 0);
-      strokeWeight(4);
-      // line(starting point x, starting point y, end point x, end point y)
-      line(points[i][0],points[i][1], points[nextindex][0],points[nextindex][1]);
+  // check if we want to erase
+  if (gesture[1] == 1) {
+    let indexPoint;
+    for (let i = 0; i < predictions.length; i += 1) {
+      const prediction = predictions[i];
+      indexPoint = prediction.annotations.indexFinger[3];
+    }
+    if (indexPoint[0] < 80 && indexPoint[1] < 80) {
+      eraseStart++;
+    } else {
+      eraseStart = 0;
+    }
+    // erase!
+    if (eraseStart > 30) {
+      //console.log("erase");
+      points = [];
     }
   }
+
+  //console.log(gesture);
+  if (onlyIndexUp(gesture)) {
+    // Call the findKeypoints function to push all keypoints into array
+    findKeypoints();
+  }
+  
+  // Draw our line
+  noFill();
+  stroke(255, 204, 0);
+  strokeWeight(4);
+  beginShape();
+  // Draw lines between keypoints within the array
+  for (let i = 0; i < points.length; i+=1){
+
+      /* CODE TO DRAW ELLIPSES
+      noStroke();
+      ellipse(arr[i][0], arr[i][1], 10); */
+
+    curveVertex(points[i][0],points[i][1]);
+  }
+  endShape();
 }
 
 // A function to push keypoints into an array
@@ -52,5 +89,33 @@ function findKeypoints() {
   for (let i = 0; i < predictions.length; i += 1) {
     const prediction = predictions[i];
     points.push(prediction.annotations.indexFinger[3]);
+    stroke(0, 0, 255);
+    circle(prediction.annotations.indexFinger[3][0], prediction.annotations.indexFinger[3][1], 5);
+  }
+}
+
+  // Function to detect gestures, i.e. which fingers are up
+  function findGesture() {
+    let out = [0, 0, 0, 0, 0];
+    // 0: false, the finger is down
+    // 1: true, the finger is up
+    // order: [thumb, index, middle, ring, pinky]
+    for (let i = 0; i < predictions.length; i += 1) {
+      const prediction = predictions[i];
+      let fingers = [prediction.annotations.thumb, prediction.annotations.indexFinger, prediction.annotations.middleFinger, prediction.annotations.ringFinger, prediction.annotations.pinky];
+      //console.log(fingers);
+      for (let i = 0; i < 5; i++) {
+        let finger = fingers[i];
+        if (finger[3][1] < finger[2][1] && finger[2][1] < finger[1][1] && finger[1][1] < finger[0][1]) {
+          // the finger is up
+          out[i] = 1;
+        }
+      }
     }
+    return out;
+  }
+
+  // Returns true if only the index is up (ignores the thumb, which can be problematic)
+  function onlyIndexUp(gesture) {
+    return ((gesture[1] == 1) && (gesture[2] == 0) && (gesture[3] == 0) && (gesture[4] == 0));
   }
